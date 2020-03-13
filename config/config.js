7540fdfeb64c5906b67e6b7372de56b25d2996f3
yunzhanghu_bj592 <yunzhanghu_bj592@yunzhanghu-bj592deMacBook-Pro.local>
2020-03-13 21:28:34 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,17 @@
+export default {
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true,
+            dva: true,
+            locale: {
+            enable: true,
+            },
+        }],
+    ],
+    singular: true,
+    routes: [{
+        path: "/",
+        component: "./HelloWorld",
+    }],
+};
+
