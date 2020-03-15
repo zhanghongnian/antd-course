b66a3fc9b8f99a886b843099afcd6d637793cef4
zhnliving <zhnliving@icloud.com>
2020-03-15 15:41:39 +0800

    add sider

--- src/page/Layout.js
+++ src/page/Layout.js
@@ -1,14 +1,30 @@
 import { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 
 const { Header, Footer, Sider, Content } = Layout;
+
+const SubMenu = Menu.SubMenu;
 
 export default class BasicLayout extends Component {
     render() {
         return (
             <Layout>
                 <Sider width={256} style={{ minHeight: '100vh', color: 'white' }}>
-                    Sider
+                    <div style={{ height: '32px', background: 'rgba(255,255,255,.2)', margin: '16px' }}> </div>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="pie-chart"/>
+                            <span>Helloworld</span>
+                        </Menu.Item>
+                        <SubMenu
+                            key="sub1"
+                            title={<span><Icon type="dashboard"/><span>Dashboard</span></span>}
+                        >
+                            <Menu.Item key="2">分析页</Menu.Item>
+                            <Menu.Item key="3">监控页</Menu.Item>
+                            <Menu.Item key="4">工作台</Menu.Item>
+                        </SubMenu>
+                    </Menu>
                 </Sider>
                 <Layout>
                     <Header style={{ background: '#fff', textAlign: 'center', padding: 0 }}>Healer</Header>
